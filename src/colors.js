5fc3350bfec2bed59b2b56e61f3ddec24539ba12
A. Enany <a.enany91@gmail.com>
2019-06-22 13:41:21 -0400

    this is more than enough

--- src/colors.js
+++ src/colors.js
@@ -1,8 +1,4 @@
-const fs = require('fs');
-const css = fs.readFileSync('./assets/css/dark-dynamic.css', 'utf-8')
-
 invert = `
-javascript: (\r\n\tfunction () { \r\n\t\/\/ the css we are going to inject\r\n\tvar css = \'html {-webkit-filter: invert(100%);\' +\r\n\t\t\'-moz-filter: invert(100%);\' + \r\n\t\t\'-o-filter: invert(100%);\' + \r\n\t\t\'-ms-filter: invert(100%); }\',\r\n\t\r\n\thead = document.getElementsByTagName(\'head\')[0],\r\n\tstyle = document.createElement(\'style\');\r\n\t\r\n\t\/\/ a hack, so you can \"invert back\" clicking the bookmarklet again\r\n\tif (!window.counter) { window.counter = 1;} else  { window.counter ++;\r\n\tif (window.counter % 2 == 0) { var css =\'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }\'}\r\n\t };\r\n\t\r\n\tstyle.type = \'text\/css\';\r\n\tif (style.styleSheet){\r\n\tstyle.styleSheet.cssText = css;\r\n\t} else {\r\n\tstyle.appendChild(document.createTextNode(css));\r\n\t}\r\n\t\r\n\t\/\/injecting the css to the head\r\n\thead.appendChild(style);\r\n\t}());\r\n
-`;
+\tfunction () {\r\n\t\tvar css = \'filter: invert(100%);\',\r\n\t\t\thead = document.getElementsByTagName(\'head\')[0],\r\n\t\t\tstyle = document.createElement(\'style\');\r\n\t\tif (!window.counter) {\r\n\t\t\twindow.counter = 1;\r\n\t\t} else {\r\n\t\t\twindow.counter++;\r\n\t\t\tif (window.counter % 2 == 0) {\r\n\t\t\t\tvar css = \'html {filter: invert(0%)}\'\r\n\t\t\t}\r\n\t\t};\r\n\r\n\t\tstyle.type = \'text\/css\';\r\n\t\tif (style.styleSheet) {\r\n\t\t\tstyle.styleSheet.cssText = css;\r\n\t\t} else {\r\n\t\t\tstyle.appendChild(document.createTextNode(css));\r\n\t\t}\r\n\t\thead.appendChild(style);\r\n\t}());\r\n`;
 
 module.exports = invert
